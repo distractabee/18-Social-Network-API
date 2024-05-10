09c18ffa73d13e57117452ec386c371678a311a1
distractabee <jssclgh@gmail.com>
2024-05-09 20:43:56 -0500

    working on Thought Controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,58 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getThoughts(req,res) {
+        Thought.find({})
+        .select('-_v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
+    },
+
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.ThoughtId })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'Head Empty, No Thoughts! '})
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
+    },
+
+    createNewThought(req, res) {
+        Thought.create(req.body)
+        .then((Thought) => res.json(Thought))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err)
+        });
+    },
+
+    editThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.ThoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No Thoughts!!!'})
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.status(400).json(err))
+    },
+
+    deleteThought(req, res) {
+        Thought.findOneAndRemove({ _id: req.params.ThoughtId })
+    }
+}
